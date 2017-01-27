337e433864864879dcb382e7d202264de8cbc902
Ash Wilson <ash.d.wilson@gmail.com>
2017-01-27 14:41:11 -0800

    chg: usr: Added a delay in Halo events monitor if results set comes back empty, better exception handling for CloudPassage API. !minor

--- app/donlib/halo_events.py
+++ app/donlib/halo_events.py
@@ -9,10 +9,11 @@ import urllib
 import utility
 from multiprocessing.dummy import Pool as ThreadPool
 from requests.exceptions import ConnectionError
+from cloudpassage import CloudPassageGeneral
 
 
 class HaloEvents(object):
-
+    """Instantiate with a donlib.ConfigHelper() object as an argument."""
     def __init__(self, config):
         self.halo_key = config.halo_api_key
         self.halo_secret = config.halo_api_secret_key
@@ -24,23 +25,29 @@ class HaloEvents(object):
         self.halo_session = None
         self.last_event_id = ""
         self.ua = config.ua
-        print("Starting event timestamp: " + self.start_timestamp)
+        print("Event Collector: Starting timestamp: " + self.start_timestamp)
 
     def __iter__(self):
+        """This allows us to iterate through the events stream."""
         while True:
-            print "Last event timestamp: %s" % str(self.last_event_timestamp)
             try:
                 for event in self.get_next_batch():
                     yield event
             except IndexError:
-                time.sleep(5)
+                pass
 
     def get_next_batch(self):
+        """Gets the next batch of events for the iterator."""
         url_list = self.create_url_list()
         try:
             pages = self.get_pages(url_list)
         except ConnectionError:  # Sometimes connection abort happens
             pages = [{"events": []}]
+        except CloudPassageGeneral:  # We wait if this happens...
+            now_string = utility.iso8601_now()
+            print("EventCollector: Caught Halo API error at: %s" % now_string)
+            pages = [{"events": []}]
+            time.sleep(15)
         events = self.events_from_pages(pages)
         if events[0]["id"] == self.last_event_id:
             del events[0]
@@ -50,10 +57,11 @@ class HaloEvents(object):
             self.last_event_id = last_event_id
             self.last_event_timestamp = last_event_timestamp
         except IndexError:
-            pass
+            time.sleep(10)
         return events
 
     def events_from_pages(self, pages):
+        """Extracts events from json, orders by timestamp."""
         events = []
         for page in pages:
             for event in page["events"]:
@@ -62,17 +70,21 @@ class HaloEvents(object):
         return result
 
     def order_events(self, events, sort_key):
+        """Sorts events for events_from_pages method."""
         sorted_list = sorted(events, key=operator.itemgetter(sort_key))
         return sorted_list
 
     def build_halo_session(self):
+        """This creates the halo session object for API interaction."""
         halo_session = cloudpassage.HaloSession(self.halo_key,
                                                 self.halo_secret,
                                                 integration_string=self.ua)
         return halo_session
 
     def create_url_list(self):
-        """We initially set the 'since' var to the start_timestamp.  The next
+        """Creates the list of URLs for the batch query.
+
+        We initially set the 'since' var to the start_timestamp.  The next
         statement will override that value with the last event's timestamp, if
         one is set
 
@@ -94,11 +106,13 @@ class HaloEvents(object):
 
     @classmethod
     def build_url(cls, base_url, modifiers):
+        """URL construction for each page"""
         params = urllib.urlencode(modifiers)
         url = "%s?%s" % (base_url, params)
         return url
 
     def get_pages(self, url_list):
+        """Map page queries to threads in a pool, return list of results."""
         halo_session = self.build_halo_session()
         page_helper = cloudpassage.HttpHelper(halo_session)
         pool = ThreadPool(self.max_threads)
