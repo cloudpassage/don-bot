525b9c4a92584989cd78debd12121ea9d531db16
Ash Wilson <ash.d.wilson@gmail.com>
2018-05-01 11:34:53 -0700

    chg: dev: Adding tests for quarantine group check. !minor

--- app/cortexlib/quarantine.py
+++ app/cortexlib/quarantine.py
@@ -26,6 +26,7 @@ class Quarantine(object):
         # If criteria are met and configuration is sane, trigger quarantine.
         if (self.criticality_match(event) is True and
                 self.event_type_match(event) is True and
+                self.group_name_match(event) is True and
                 self.config_is_unambiguous() is True):
             return event
         # Don't trigger by default.
@@ -84,6 +85,16 @@ class Quarantine(object):
     def event_type_match(self, event):
         """Return boolean for event type match."""
         if event["type"] in self.quarantine_trigger_events:
+            return True
+        else:
+            return False
+
+    def group_name_match(self, event):
+        """Return bool for server group name match."""
+        # If this is not a workload event, return False
+        if not event["server_group_name"]:
+            return False
+        if event["server_group_name"] in self.quarantine_trigger_group_names:
             return True
         else:
             return False
--- app/test/integration/test_integration_cortexlib_quarantine.py
+++ app/test/integration/test_integration_cortexlib_quarantine.py
@@ -151,3 +151,11 @@ class TestIntegrationCortexlibQuarantine:
         q_event["server_group_name"] = q.quarantine_trigger_group_names[0]
         q_event["type"] = q.quarantine_trigger_events[0]
         assert q.should_quarantine(quar_event) is False
+
+    def test_quarantine_event_no_trigger_bad_group(self, monkeypatch):
+        """Quarantine fails because group name does not match."""
+        q = self.instantiate_cortexlib_quarantine(monkeypatch)
+        q_event = quar_event.copy()
+        q_event["server_group_name"] = "Not a target group name"
+        q_event["type"] = q.quarantine_trigger_events[0]
+        assert q.should_quarantine(q_event) is False
